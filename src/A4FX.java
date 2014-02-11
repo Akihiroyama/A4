3321777d7c1955b71d37d827c1d5107878a7bb97
Akihiroyama <hiroyama.1988@gmail.com>
2014-02-11 21:27:16 +0900

    #02/11 add  textArea.requestFocus
    ダイアログを閉じた後、OK･Clearボタンをクリック後にテキストエリアを選択状態になるよう変更。
    再びテキストエリアを選択する手間を省き、すぐに文字列を編集可能にするため。

--- src/A4FX.java
+++ src/A4FX.java
@@ -18,70 +18,69 @@ import javax.swing.JOptionPane;
  */
 public class A4FX extends Application {
    
-    private VBox vbox;
-    private Scene scene;
-    private TextArea textArea;
-    private Label label;
-    private Button commandButton;
-    private Button commandButton1;
-    private HBox commandArea;
-    private String string1;
-    private String new_string1; 
-    private String new_string2;
+	private VBox vbox;
+	private Scene scene;
+	private TextArea textArea;
+	private Label label;
+	private Button commandButton;
+	private Button commandButton1;
+	private String string1;
+	private String new_string1; 
+	private String new_string2;
   
    
-    @Override
-    public void start(Stage stage) {
-        vbox = new VBox();
-        scene = new Scene(vbox);
-        textArea = new TextArea();
-	label = new Label("3文字以上入力してください！");
-        commandButton = new Button("OK");
-        commandButton1 = new Button("Clear");
-        commandArea = new HBox();
-        commandArea.setSpacing(5);
-        string1 = textArea.getText();
+	@Override
+	public void start(Stage stage) {
+		vbox = new VBox();
+		scene = new Scene(vbox);
+		textArea = new TextArea();
+		label = new Label("3文字以上入力してください！");
+		commandButton = new Button("OK");
+		commandButton1 = new Button("Clear");
+		commandArea = new HBox();
+		commandArea.setSpacing(5);
+		string1 = textArea.getText();
      
         
-        commandArea.getChildren().addAll(commandButton, commandButton1);
-        vbox.getChildren().addAll(label,textArea, commandArea);
+	commandArea.getChildren().addAll(commandButton, commandButton1);
+	vbox.getChildren().addAll(label,textArea, commandArea);
  
    
-        commandButton.setOnAction(new EventHandler<ActionEvent>(){
-        @Override
-        public void handle(ActionEvent actionEvent) {
-            string1 = textArea.getText();
-            int i=2;
-             if(string1.length() <= i){
-                JOptionPane.showMessageDialog(null, "正しく入力して！");
-                 
-             }else if(i < string1.length()){
-                 new_string1 = string1.substring(0,3);
-                 new_string2 = string1.substring(3);
-                 textArea.setText(new_string1+ "waja"+new_string2);
-             }
-        }
-      
-    });
+	commandButton.setOnAction(new EventHandler<ActionEvent>(){
+	@Override
+	public void handle(ActionEvent actionEvent) {
+		string1 = textArea.getText();
+		int i=2;
+		if(string1.length() <= i){
+			JOptionPane.showMessageDialog(null, "正しく入力して！");
+			textArea.requestFocus();
+			
+			}else if(i < string1.length()){
+				new_string1 = string1.substring(0,3);
+				new_string2 = string1.substring(3);
+				textArea.setText(new_string1+ "waja"+new_string2);
+				textArea.requestFocus();
+			}
+		}
+	});
    
-         commandButton1.setOnAction(new EventHandler<ActionEvent>(){
-        @Override
-        public void handle(ActionEvent actionEvent) {
+	commandButton1.setOnAction(new EventHandler<ActionEvent>(){
+	@Override
+	public void handle(ActionEvent actionEvent) {
 
-            textArea.setText("");
-           
-        }
-    });
-         
-    stage.setScene(scene);
-    stage.setTitle("JavaFX2");
-    stage.setWidth(500); stage.setHeight(300);
-    stage.show();
-  }
+		textArea.setText("");
+		textArea.requestFocus();
+		}
+	});
+	
+	stage.setScene(scene);
+	stage.setTitle("JavaFX2");
+	stage.setWidth(500); stage.setHeight(300);
+	stage.show();
+	}
 
-    public static void main(String[] args) {
-        Application.launch(A4FX.class, args);
-      
-    }
-    
+	public static void main(String[] args) {
+	Application.launch(A4FX.class, args);
+	
+	}
 }
